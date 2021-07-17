838dc3acb75801c33d59354a13ed855f1b5b475c
siukibo <50410461+siukibo@users.noreply.github.com>
2021-07-17 22:02:20 +0700

    learn
    learned

---
+++ Razor05/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Razor05.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+
+        public string SayGoodbye() => $"Hello, {FullName}! I'm in the model class";
+
+        public void OnGet()
+        { 
+                FirstName = "Donald";
+                LastName = "Trump";
+            
+        }
+    
+
+    }
+}
---
+++ Razor05/Pages/ViewData.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Razor05.Pages
+{
+    public class ViewDatasModel : PageModel
+    {
+            
+        
+            public void OnGet()
+            {
+                ViewData["MyNumber"] = 2020;
+                ViewData["MyString"] = "Hello World, Razor Pages!";
+                ViewData["MyObject"] = new Book{
+                    Title = "ASP.NET Core for Dummy",
+                    Publisher = "Tự học ICT Press",
+                    Author = "Mai Chi"
+                };
+            }
+    }
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+        public string Author { get; set; }
+
+    }
+}
